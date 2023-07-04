1ab7860d51b0888b3c631f35a442c64b63da61eb
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-04 12:01:00 -0300

    Criacao dos componentes Header, Sidebar e configurada estrutura inicial com os posicionamentos dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
   return (
     <>
-      <div>TESTE</div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>conteudo</main>
+      </div>
     </>
   );
 }
