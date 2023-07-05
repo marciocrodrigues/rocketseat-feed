f847fe89aaf3a98f51dd2503dc9c5c88b7f08b6a
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-04 21:25:27 -0300

    componente post com estilizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>conteudo</main>
+        <main>
+          <Post />
+        </main>
       </div>
     </>
   );
---
+++ src/components/Post.jsx
@@ -0,0 +1,44 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <div>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img src="https://github.com/getomni.png" />
+            <div className={styles.authorInfo}>
+              <strong>Teste dos Santos</strong>
+              <span>Web Developer</span>
+            </div>
+          </div>
+
+          <time title="4 de Julho de 2023 às 20:54" dateTime="2023-07-23 20:53">
+            Publicado há 1h
+          </time>
+        </header>
+
+        <div className={styles.content}>
+          <p>Fala galeraa</p>
+          <p>Acabei de subir mais um projeto no meu portifa.</p>
+          <p>
+            <a href="#">jane.design/doctorcare</a>
+          </p>
+          <p>
+            <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+            <a href="#">#rocketseat</a>
+          </p>
+        </div>
+
+        <form className={styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
+
+          <textarea placeholder="Deixe seu comentario"></textarea>
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
+      </article>
+    </div>
+  );
+}
